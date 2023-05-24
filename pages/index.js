e64a560b92b66781b2891b9f3b9ebec0ae4c20c5
jemi-ma <131380161+jemi-ma@users.noreply.github.com>
2023-05-24 12:22:43 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+import { useRouter } from 'next/router';
+export default function Home() {
+  const router = useRouter();
+  return (<h1> {router.query.name ? `Hello, ${router.query.name}` : "I don't know your name"} </h1> ); 
+}
+ 
